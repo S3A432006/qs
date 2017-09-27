ebf11587ea652be6946dc0cd256e69f561a68ad1
Jia-Yi Lai <anniuu7134@gmail.com>
2017-09-27 12:30:44 +0800

    新增layouts目錄->新增app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/9/27
- * Time: 上午 11:52
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+
+    <!-- CSS 及 JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
